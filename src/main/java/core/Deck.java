9185b15cd45c9c8ea3f62b574d38e7d80fd450ad
sinaesmaeili <sina.esmaeili01@gmail.com>
2018-10-06 16:35:14 -0400

    Code for Deck class tests

---
+++ src/main/java/core/Deck.java
@@ -0,0 +1,40 @@
+package core;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Deck {
+
+    private static final char[] COLOURS = {'R', 'B', 'G', 'O'};
+    private static final int[] RANKS = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
+    private ArrayList<Tile> deck = new ArrayList<Tile>();
+
+    public Deck() {
+        this.generateDeck();
+        this.shuffleDeck();
+    }
+
+    public void generateDeck() {
+        // Outer loop for creating every tile twice
+        for (int x = 0; x < 2; x++) {
+            for (int i = 0; i < COLOURS.length; i++) {
+                for (int j = 0; j < RANKS.length; j++) {
+                    Tile tile = new Tile(COLOURS[i], RANKS[j]);
+                    deck.add(tile);
+                }
+            }
+        }
+    }
+
+    public void shuffleDeck() {
+        Collections.shuffle(deck);
+    }
+
+    public Tile drawTile() {
+        return deck.remove(0);
+    }
+
+    public int getDeckSize() {
+        return deck.size();
+    }
+}
