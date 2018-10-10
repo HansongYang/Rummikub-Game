c594c8642560f7cc2be455ec2bd1c9c950ea07f1
Hansong Yang <hansongyang@Hansongs-MacBook-Pro.local>
2018-10-09 20:34:00 -0400

    Refactor the JUnit tests for HandTest class

--- src/test/java/core/HandTest.java
+++ src/test/java/core/HandTest.java
@@ -84,10 +84,10 @@ public class HandTest extends TestCase{
 		hand.add(t3);
 		ArrayList<Meld> set = hand.getMeldSets();
 		
-		assertTrue(3 == set.size());
-		assertTrue(3 == set.get(0).getTile(0).getRank() && 'G' == set.get(0).getTile(0).getColour());
-		assertTrue(3 == set.get(0).getTile(1).getRank() && 'B' == set.get(1).getTile(0).getColour());
-		assertTrue(3 == set.get(0).getTile(2).getRank() && 'O' == set.get(2).getTile(0).getColour());
+		assertTrue(3 == set.get(0).size());
+		assertTrue(3 == set.get(0).getTile(0).getRank() && 'O' == set.get(0).getTile(0).getColour());
+		assertTrue(3 == set.get(0).getTile(1).getRank() && 'G' == set.get(0).getTile(1).getColour());
+		assertTrue(3 == set.get(0).getTile(2).getRank() && 'B' == set.get(0).getTile(2).getColour());
 	}
 	
 	public void testGetMeldRuns() {
@@ -104,7 +104,7 @@ public class HandTest extends TestCase{
 		hand.add(t3);
 		ArrayList<Meld> run = hand.getMeldRuns();
 		
-		assertTrue(3 == run.size());
+		assertTrue(3 == run.get(0).size());
 		assertTrue(1 == run.get(0).getTile(0).getRank() && 'R' == run.get(0).getTile(0).getColour());
 		assertTrue(2 == run.get(0).getTile(1).getRank() && 'R' == run.get(0).getTile(1).getColour());
 		assertTrue(3 == run.get(0).getTile(2).getRank() && 'R' == run.get(0).getTile(2).getColour());
