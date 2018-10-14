765c1998073806cdceee7f49c3849a7c86a60ef3
sinaesmaeili <sina.esmaeili01@gmail.com>
2018-10-14 14:40:50 -0400

    Test cases for board, game, and meldvalidatorservice Disclaimer: code in meldvalidatorservice and meldvalidator service is michaels. Moving code to seperate class for simplicity

--- src/test/java/core/BoardTest.java
+++ src/test/java/core/BoardTest.java
@@ -15,6 +15,41 @@ public class BoardTest extends TestCase {
 		board.addMeld(meld);
 		
 		assertEquals(1, board.currentMelds.size());
-		
+	}
+
+	public void testAddTileToMeldEnd() {
+		Board board = new Board();
+
+		Meld meld = new Meld();
+		meld.add(new Tile('R', 9));
+		meld.add(new Tile('R', 10));
+		meld.add(new Tile('R', 11));
+		board.addMeld(meld);
+
+		Meld newMeld = new Meld();
+		newMeld.add(new Tile('R', 12));
+		newMeld.add(new Tile('R', 13));
+
+		board.addTileToMeld(0, 3, newMeld);
+		assertEquals(12, board.currentMelds.get(0).getTile(3).getRank());
+		assertEquals(13, board.currentMelds.get(0).getTile(4).getRank());
+	}
+
+	public void testAddTileToMeldBeginning() {
+		Board board = new Board();
+
+		Meld meld = new Meld();
+		meld.add(new Tile('R', 9));
+		meld.add(new Tile('R', 10));
+		meld.add(new Tile('R', 11));
+		board.addMeld(meld);
+
+		Meld newMeld = new Meld();
+		newMeld.add(new Tile('R', 7));
+		newMeld.add(new Tile('R', 8));
+
+		board.addTileToMeld(0, -1, newMeld);
+		assertEquals(7, board.currentMelds.get(0).getTile(0).getRank());
+		assertEquals(8, board.currentMelds.get(0).getTile(1).getRank());
 	}
 }
