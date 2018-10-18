b76f0900e97bad08a282811085a00644ab9ef942
sinaesmaeili <sina.esmaeili01@gmail.com>
2018-10-18 13:31:20 -0400

    Code for new board features and modifications - board function for taking tile on board to form new meld - small modifications to tile, meld, hand, and game classes

--- src/main/java/core/Board.java
+++ src/main/java/core/Board.java
@@ -11,57 +11,90 @@ public class Board {
     public Board() { }
     
     public void addMeld(Meld meld) {
-    	currentMelds.add(meld);
+    	meld.setJustPlayedFlagTrue();
+        currentMelds.add(meld);
     }
 
-    // Function that first adds { Meld meld } param into { rowLocation} of currentMelds and validates it
-    // if valid it get saved into currentMelds
-    // works for adding melds to end, and beginning
-    public void addTileToMeld(int rowLocation, int tileMeldLocation, Meld meld) {
+    /**
+     * Function that adds new meld to end of a current meld
+     *
+     * @param rowLocation Number where existing meld is located on the board
+     * @param meld New meld being added to the board
+     * @return boolean
+     */
+    public boolean addTileToMeldEnd(int rowLocation, Meld meld) {
         Meld checkMeld = currentMelds.get(rowLocation);
 
         for (int i = 0; i < meld.size(); i++) {
-            if (tileMeldLocation == meld.size()) { // for adding meld to the end
-                checkMeld.add(meld.getTile(i));
-            } else if (tileMeldLocation == -1) { // for adding meld to beginning
-                checkMeld.add(i, meld.getTile(i));
-            }
+            checkMeld.add(meld.getTile(i));
         }
 
         if (meldValidatorService.isValidMeld(checkMeld.getTiles())) {
+            checkMeld.setMovedToFormNewFlagTrue();
             currentMelds.set(rowLocation, checkMeld);
+            return true;
         } else {
-            System.out.println("Invalid Meld");
+            return false; // Invalid meld created
         }
     }
 
-//	public void addToExistingMelds() {
-//		Scanner reader = new Scanner(System.in);
-//
-//		game.getBoard().printBoard();
-//
-//		while (true) {
-//			System.out.println("Pick row number from existing melds on board. (-1) to cancel");
-//			int rowNum = reader.nextInt();
-//
-//			if (rowNum == -1) {
-//				break;
-//			} else if (rowNum > game.getBoard().currentMelds.size()-1 || rowNum < 0) {
-//				System.out.println("Invalid row number, try again");
-//			} else {
-//				System.out.println("Pick location to place tile in existing meld: ");
-//				int tileMeldLocation = reader.nextInt();
-//
-//				if (tileMeldLocation > game.getBoard().currentMelds.size()-1 || tileMeldLocation < 0) {
-//					System.out.println("Invalid tile number, try again");
-//				} else {
-//					game.getBoard().addTileToMeld(rowNum, tileMeldLocation, )
-//				}
-//			}
-//
-//
-//		}
-//	}
+    /**
+     * Function that adds new meld to beginning of a current meld
+     *
+     * @param rowLocation Number where existing meld is located on the board
+     * @param meld New meld being added to the board
+     * @return boolean
+     */
+    public boolean addTileToMeldBeginning(int rowLocation, Meld meld) {
+        Meld checkMeld = currentMelds.get(rowLocation);
+
+        for (int i = 0; i < meld.size(); i++) {
+            checkMeld.add(i, meld.getTile(i));
+        }
+
+        if (meldValidatorService.isValidMeld(checkMeld.getTiles())) {
+            checkMeld.setMovedToFormNewFlagTrue();
+            currentMelds.set(rowLocation, checkMeld);
+            return true;
+        } else {
+            return false; // Invalid meld created
+        }
+    }
+
+
+    /**
+     * Function for taking an existing meld and adding it to new meld to play on board
+     *
+     * @param rowLocation Location of meld to take tile from
+     * @param tileMeldLocation Tile location of meld on board
+     * @param tileLocationForNewMeld Where to place newly received tile into new meld
+     * @param meld New meld just generated
+     * @return boolean
+     */
+    public boolean takeTileToFormNewMeld(int rowLocation, int tileMeldLocation, int tileLocationForNewMeld, Meld meld) {
+        Meld checkMeld = currentMelds.get(rowLocation);
+        Meld newMeld = meld;
+
+        Tile removedTile = checkMeld.remove(tileMeldLocation);
+        if (removedTile != null && meldValidatorService.isValidMeld(checkMeld.getTiles())) {
+
+            if (tileLocationForNewMeld >= newMeld.size()) {
+                newMeld.add(removedTile);
+            } else {
+                newMeld.add(tileLocationForNewMeld, removedTile);
+            }
+
+            if (meldValidatorService.isValidMeld(newMeld.getTiles())) {
+                newMeld.setMovedToFormNewFlagTrue();
+                currentMelds.add(newMeld);
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 
     public void printBoard() {
     	System.out.println("Board: ");
