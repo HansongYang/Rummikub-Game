7fd699fb05bd0d2311403f1806a22042600a9eb4
Ryan <ryanyduan@gmail.com>
2018-11-22 19:33:49 -0500

    Added arraylist of arraylist of tiles (sets and runs) to userstrategy.  This will be the arraylist of 'recommendations' to the human of what tiles to play

--- src/main/java/core/UserPlayer.java
+++ src/main/java/core/UserPlayer.java
@@ -22,6 +22,77 @@ public class UserPlayer extends Player{
 		meldsInHand.clear();
 	}
 	
+	public ArrayList<ArrayList<Tile>> findRuns(){
+		
+		// finds runs in player hand
+		// note that if 12345 is in the hand, findSets() will only return 12345
+		// it will not return 123, 234, 345, 2345, 1234
+
+		ArrayList<ArrayList<Tile>> runs = new ArrayList<ArrayList<Tile>>();
+		ArrayList<Tile> matchedTiles = new ArrayList<Tile>();
+		char initialColour;
+		for (Tile t: this.hand.getTiles()) {
+			if (matchedTiles.contains(t)) continue; //if we've found a run using this tile, don't use it to find more runs
+		
+			ArrayList<Tile> possibleRunList = new ArrayList<Tile>();
+			
+			possibleRunList.add(t);
+			initialColour = t.getColour();
+		
+			int currentNum = t.getRank();
+			for (Tile possibleTile: this.hand.getTiles()) {
+				if (possibleTile.getColour() == initialColour) {
+					if (possibleTile.getRank() == currentNum+1) {
+						possibleRunList.add(possibleTile);
+						currentNum = possibleTile.getRank();
+					}
+					else if (possibleTile.getRank() == currentNum) {
+						continue;
+					}
+				}
+			}
+			
+			if (possibleRunList.size() >= 3) {
+				runs.add(possibleRunList); 
+				for (Tile matched : possibleRunList) matchedTiles.add(matched);
+			}
+		}
+		return runs;
+	}
+	
+	public ArrayList<ArrayList<Tile>> findSets(){
+		
+		// finds sets in player hand
+		// note that if 4444 is in the hand, findSets() will only return 4444
+		// it will not return 444 444 444
+		
+		ArrayList<ArrayList<Tile>> sets = new ArrayList<ArrayList<Tile>>();
+		ArrayList<Tile> matchedTiles = new ArrayList<Tile>();
+		
+		for (Tile t: this.hand.getTiles()) {
+			if (matchedTiles.contains(t)) continue; // if we've found a set using this tile, don't use it to find more sets
+			ArrayList<Tile> possibleSetList = new ArrayList<Tile>();
+			
+			possibleSetList.add(t);
+			ArrayList<Character> prevColours = new ArrayList<Character>(); 
+			prevColours.add(t.getColour());
+			for (Tile possibleTile: this.hand.getTiles()) {
+				if (possibleTile.getRank() == t.getRank()) {
+					if (!prevColours.contains(possibleTile.getColour())) {
+						possibleSetList.add(possibleTile);
+						prevColours.add(possibleTile.getColour());
+					}
+				}
+			}
+		
+			if (possibleSetList.size() >= 3) {
+				sets.add(possibleSetList);
+				for (Tile matched: possibleSetList) matchedTiles.add(matched);
+			}
+		}
+		return sets;
+	}
+	
 	public Meld createMeld(ArrayList<Tile> selectedTiles, Hand availableTiles) {
 		
 		if(meldValidatorService.isValidMeld(selectedTiles)) {
