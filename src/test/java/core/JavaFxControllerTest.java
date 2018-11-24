2c88adce0b14427e310cb921ef62b573f2e068e4
sinaesmaeili <sina.esmaeili01@gmail.com>
2018-11-24 11:11:30 -0500

    Test cases for memento in controller

---
+++ src/test/java/core/JavaFxControllerTest.java
@@ -0,0 +1,30 @@
+package core;
+
+import junit.framework.TestCase;
+
+public class JavaFxControllerTest extends TestCase {
+
+    public void testMemento() {
+        Model model = new Model();
+        String[] strategy = {"AI Strategy 1"};
+
+        model.setNumPlayer(2);
+        model.setStrategy(strategy);
+        model.initGame();
+
+        JavaFxController controller = new JavaFxController(model);
+        controller.saveGame();
+
+        assertTrue(controller.model.getBoard().currentMelds.isEmpty());
+
+        Meld meld = new Meld();
+        meld.add(controller.drawTile());
+        controller.model.getBoard().addMeld(meld);
+
+        assertFalse(controller.model.getBoard().currentMelds.isEmpty());
+
+        controller.restoreGame();
+
+        assertTrue(controller.model.getBoard().currentMelds.isEmpty());
+    }
+}
